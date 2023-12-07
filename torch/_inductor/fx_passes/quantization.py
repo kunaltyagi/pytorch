399b9c05c4ee1076bfda9d6ced7dc265a621cfa0
jiayisun <jiayi.sun@intel.com>
2023-12-06 17:40:55 -0800

    correct format

--- torch/_inductor/fx_passes/quantization.py
+++ torch/_inductor/fx_passes/quantization.py
@@ -427,7 +427,10 @@ def _is_valid_quantized_conv_binary_optimization_pattern(output_dtype):
         if output_dtype is not None:
             binary_node_inputs = list(qconv2d_node_after_weight_prepack.users)[0].args
             assert len(binary_node_inputs) == 2, "Expects binary node with 2 inputs"
-            if binary_node_inputs[0].meta["val"].size() != binary_node_inputs[1].meta["val"].size():
+            if (
+                binary_node_inputs[0].meta["val"].size()
+                != binary_node_inputs[1].meta["val"].size()
+            ):
                 return False
             extra_input_node = None
             for arg in binary_node_inputs:
