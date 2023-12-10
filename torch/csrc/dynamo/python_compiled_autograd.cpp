4f9ef7dc6f15b261b23680074c545e1583d464e1
voznesenskym <voznesenskym@gmail.com>
2023-12-09 17:18:03 -0800

    FSDP initial
    ghstack-source-id: c69117cd83504064fd1dd6a830227c74544cbdd7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115410
    
    make it work
    
    make it work
    
    Fixes
    
    Rm
    
    rms
    
    rm

--- torch/csrc/dynamo/python_compiled_autograd.cpp
+++ torch/csrc/dynamo/python_compiled_autograd.cpp
@@ -8,6 +8,7 @@
 #include <torch/csrc/utils/pythoncapi_compat.h>
 #include <iostream>
 #include <vector>
+#include <ATen/SparseCsrTensorUtils.h>
 
 /*
 [Note: Compiled Autograd]
@@ -425,6 +426,7 @@ variable_list compiled_autograd(
 
       saved.debug_asserts();
       saved.before(call.node->next_edges());
+
       validate_outputs(
           call.node->next_edges(), outputs, [&](const std::string& msg) {
             std::ostringstream ss;
